ba602a7a1b35662a1c059b4dddb9c0e7a15725b0
Dan Dzoan <dandzoan@gmail.com>
2020-07-29 00:14:05 -0700

    update cells

--- src/App.js
+++ src/App.js
@@ -26,6 +26,37 @@ function App() {
           if(highlightIndex % GRIDSIZE < GRIDSIZE - 1)
             setHighlightIndex(highlightIndex + 1);
           break;
+        case "a":
+        case 'b':
+        case 'c':
+        case 'd':
+        case 'e':
+        case 'f':
+        case 'g':
+        case 'h':
+        case 'i':
+        case 'j':
+        case 'k':
+        case 'l':
+        case 'm':
+        case 'n':
+        case 'o':
+        case 'p':
+        case 'q':
+        case 'r':
+        case 's':
+        case 't':
+        case 'u':
+        case 'v':
+        case 'w':
+        case 'x':
+        case 'y':
+        case 'z':
+          const newCells = [...cells];
+          newCells[highlightIndex] = event.key;
+          console.log(newCells)
+          setCells(newCells);
+          break;
         default:
           break;
       }
